98a25f6c5408ed40cc04528867655e42f34b588e
InWamos <mykyta.pidgornyi@gmail.com>
2024-07-13 00:56:03 +0300

    fix adding of channel name to the output

--- export/parser.py
+++ export/parser.py
@@ -6,4 +6,4 @@ def find_all_messages(file_content: str) -> list[str]:
     all_messages_fields = soup.find_all(class_="text")
     messages = list(message.get_text(strip=True) for message in all_messages_fields)
 
-    return messages
+    return messages[1:]
